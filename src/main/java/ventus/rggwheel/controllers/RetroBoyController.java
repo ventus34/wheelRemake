83c29b9f06818fb70b1cec758308b16f243fb84a
ventus <jedrzej.dziegielewski@gmail.com>
2021-04-02 21:46:59 +0200

    Moved controller

---
+++ src/main/java/ventus/rggwheel/controllers/RetroBoyController.java
@@ -0,0 +1,58 @@
+package ventus.rggwheel.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+
+public class RetroBoyController {
+
+    @FXML
+    private void addInv(){}
+    @FXML
+    private void useInv(){}
+    @FXML
+    private void addHint(){}
+    @FXML
+    private void useHint(){}
+    @FXML
+    private void addChat(){}
+    @FXML
+    private void useChat(){}
+    @FXML
+    private void addReroll(){}
+    @FXML
+    private void useReroll(){}
+
+    public void spinAction(ActionEvent actionEvent) {
+    }
+
+    public void moveUp(ActionEvent actionEvent) {
+    }
+
+    public void moveDown(ActionEvent actionEvent) {
+    }
+
+    public void timeUp(ActionEvent actionEvent) {
+    }
+
+    public void timeDown(ActionEvent actionEvent) {
+    }
+
+    public void checkPrize(ActionEvent actionEvent) {
+    }
+
+    public void reset(MouseEvent mouseEvent) {
+    }
+
+    public void pickColor(ActionEvent actionEvent) {
+    }
+
+    public void start(ActionEvent actionEvent) {
+    }
+
+    public void switchMode(ActionEvent actionEvent) {
+    }
+
+    public void generateStats(ActionEvent actionEvent) {
+    }
+}
